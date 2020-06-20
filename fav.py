592b3a81825395f2a63e3a6854bdb4f10fbd5085
CommanderRag <67055837+CommanderRag@users.noreply.github.com>
2020-06-20 18:35:36 +0530

    Update fav.py

--- fav.py
+++ fav.py
@@ -1,6 +1,5 @@
 import os
 import re
-#import urlextract
 import sys
 import time
 import requests
@@ -8,36 +7,37 @@ import requests
 from bs4 import BeautifulSoup
 from notify_run import Notify
 
-
-notify=Notify()
+notify = Notify()
 print(notify.register())
 time.sleep(40)
 
 print("working")
 
-url="https://4anime.to/boruto-naruto-next-generations-episode-155?id=33531"
-r=requests.get(url)
-html=r.content
-soup=BeautifulSoup(html,'html.parser')
-text=soup.find_all(text=True)
-output="" 
-blacklist=[
-          '[document]','noscript','header','html','meta','head','input','script'
-          ]
+url = "https://4anime.to/boruto-naruto-next-generations-episode-155?id=33531"
+r = requests.get(url)
+html = r.content
+soup = BeautifulSoup(html, 'html.parser')
+text = soup.find_all(text=True)
+output = ""
+blacklist = [
+    '[document]', 'noscript', 'header', 'html', 'meta', 'head', 'input', 'script'
+]
 for t in text:
-        if t.parent.name not in blacklist:
-         output+='{}'.format(t)
+    if t.parent.name not in blacklist:
+        output += '{}'.format(t)
 
- 
-if(output.find("Episode 155")!=-1):
-       notify.send("Episode 155 is out now!")
-       sys.exit()
+while output.find("Episode 155") == -1:
+    notify.send("Episode 155 is not out yet!")
+    print("executed script")
 
+    time.sleep(86400)
 else:
-       notify.send("Episode 155 is not out yet!")
-       print("executed script")
-       date_time = time.strftime("%b %d %Y %-I:%M %p")
-       print(date_time)
-       time.sleep(86400)
-       os.execl(sys.executable, sys.executable, * sys.argv)
-       
+    notify.send("Episode 155 is out now!")
+    sys.exit()
+
+# notify.send("Episode 155 is not out yet!")
+# print("executed script")
+# date_time = time.strftime("%b %d %Y %-I:%M %p")
+# print(date_time)
+# time.sleep(86400)
+# os.execl(sys.executable, sys.executable, * sys.argv)
