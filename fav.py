1ee98f57842dd8192a8fd01eb46c481474e6e701
CommanderRag <67055837+CommanderRag@users.noreply.github.com>
2020-06-21 19:09:19 +0530

    Update fav.py

--- fav.py
+++ fav.py
@@ -29,8 +29,20 @@ for t in text:
 while output.find("Episode 155") == -1:
     notify.send("Episode 155 is not out yet!")
     print("executed script")
-
+    
     time.sleep(86400)
+    url = "https://4anime.to/boruto-naruto-next-generations-episode-155?id=33531"
+    r = requests.get(url)
+    html = r.content
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.find_all(text=True)
+    output = ""
+    blacklist = [
+    '[document]', 'noscript', 'header', 'html', 'meta', 'head', 'input', 'script'
+                ]
+    for t in text:
+        if t.parent.name not in blacklist:
+        output += '{}'.format(t)
 else:
     notify.send("Episode 155 is out now!")
     sys.exit()
