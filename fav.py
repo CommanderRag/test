d582775bb308198df8323fc5115eb7aa7ae77cfa
CommanderRag <67055837+CommanderRag@users.noreply.github.com>
2020-07-19 17:23:00 +0530

    Update fav.py

--- fav.py
+++ fav.py
@@ -5,15 +5,11 @@ import time
 import requests
 
 from bs4 import BeautifulSoup
-from notify_run import Notify
 
-notify = Notify()
-print(notify.register())
-time.sleep(50)
-sleep=86400
 print("working")
-
-url = "https://4anime.to/boruto-naruto-next-generations-episode-155?id=33531"
+number=variables['number']
+print(number)
+url = "https://4anime.to/boruto-naruto-next-generations-episode-{}".format(number)
 r = requests.get(url)
 html = r.content
 soup = BeautifulSoup(html, 'html.parser')
@@ -26,27 +22,12 @@ for t in text:
     if t.parent.name not in blacklist:
         output += '{}'.format(t)
 
-while output.find("Episode 155") == -1:
-    notify.send("Episode 155 is not out yet!")
-    print("executed script")
-    
-    time.sleep(sleep)
-    sleep=sleep-18000
-    if sleep<=10800:
-        sleep=10800
-    url = "https://4anime.to/boruto-naruto-next-generations-episode-155?id=33531"
-    r = requests.get(url)
-    html = r.content
-    soup = BeautifulSoup(html, 'html.parser')
-    text = soup.find_all(text=True)
-    output = ""
-    blacklist = [
-    '[document]', 'noscript', 'header', 'html', 'meta', 'head', 'input', 'script'
-                ]
-    for t in text:
-        if t.parent.name not in blacklist:
-            output += '{}'.format(t)
-else:
-    notify.send("Episode 155 is out now!")
-    sys.exit()
+if(output.find("Episode {}".format(number)))==-1:
+  message="Episode {} is not out yet!".format(number)
+  response = requests.post('https://notify.run/4S9uUexUL6gEhHZu', data=message)
 
+  
+  
+else:
+  message="Episode {} is out now!".format(number)
+  response = requests.post('https://notify.run/4S9uUexUL6gEhHZu', data=message)
